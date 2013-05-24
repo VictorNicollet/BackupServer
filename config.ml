8fa3cd7402c79d5c3050a149eb149373df5d3d0f
Victor Nicollet <victor@nicollet.net>
2013-05-24 15:48:01 +0200

    [Config] Initial version

---
+++ config.ml
@@ -0,0 +1,4 @@
+(** Configuration file. Edit this file to run the backup server on your own. *)
+
+(** Source server. This is where the remote CouchDB database runs. *)
+let remote_couchdb_server = "prod-01.runorg.com"
